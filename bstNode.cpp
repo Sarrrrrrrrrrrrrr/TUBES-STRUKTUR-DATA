c6fbe53625b6c019e61018921414d789ba651300
Abisar Fathir <abisarfathir7@gmail.com>
2025-12-18 12:29:27 +0700

    Create bstNode.cpp

---
+++ bstNode.cpp
@@ -0,0 +1,41 @@
+#include "bstNode.h"
+
+
+address createNode(int objectX, int objectY, string namaObject, string pesanObject, bool statusDilewati) {
+    address node = new Node;
+    node->x = objectX;
+    node->y = objectY;
+    node->nama = namaObject;
+    node->pesan = pesanObject;
+    node->tembus = statusDilewati;
+    node->left = node->right = NIL;
+    return node;
+}
+
+address insert(address root, int x, int y, string namaObj, string pesanObj, bool statusDilewati){
+    if (root == NIL) {
+        root = createNode(x, y, namaObj, pesanObj, statusDilewati);
+        return root;
+    }
+
+    // Prioritas X, lalu Y (sederhana)
+    if (x < root->x) root->left = insert(root->left, x, y, namaObj, pesanObj, statusDilewati);
+    else if (x > root->x) root->right = insert(root->right, x, y, namaObj, pesanObj, statusDilewati);
+    else {
+        if (y < root->y) root->left = insert(root->left, x, y, namaObj, pesanObj, statusDilewati);
+        else root->right = insert(root->right, x, y, namaObj, pesanObj, statusDilewati);
+    }
+    return root;
+}
+
+address cariNode(address root, int x, int y) {
+    if (root == NIL) return NIL;
+    if (root->x == x && root->y == y) return root;
+
+    if (x < root->x) return cariNode(root->left, x, y);
+    else if(x > root->x) return cariNode(root->right, x, y);
+    else {
+        if (y < root->y) return cariNode(root->left, x, y);
+        else return cariNode(root->right, x, y);
+    }
+}
